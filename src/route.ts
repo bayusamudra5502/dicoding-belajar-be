d82d22d009c16199311ea9fc00df618aae8d63e8
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2022-06-07 08:05:23 +0700

    Menambahkan payload

--- src/route.ts
+++ src/route.ts
@@ -1,4 +1,10 @@
 import * as Hapi from "@hapi/hapi"
+import * as crypto from "node:crypto"
+
+interface payload {
+  username: string,
+  password: string
+}
 
 export default function RouteRegister(server: Hapi.Server) {
   server.route({
@@ -12,17 +18,47 @@ export default function RouteRegister(server: Hapi.Server) {
   })
   
   server.route({
-    path: "/about",
+    path: "/users/{name}",
     method:"*",
     handler: (req, res) => {
+      const {name} = req.params;
       return {
-        "message": "About Page"
+        "status": "OK",
+        username: name
       }
     }
   })
 
   server.route({
-    path: "/{any*}",
+    path: "/search",
+    method: "GET",
+    handler: (req, res) => {
+      const {q} = req.query;
+
+      return {
+        status: "OK",
+        query: q
+      }
+    }
+  })
+
+  server.route({
+    path: "/login",
+    method: "POST",
+    handler: (req,res) => {
+      const {username, password} = req.payload as payload;
+      const hash = crypto.createHash("sha512");
+
+      return {
+        status: "OK",
+        message: username,
+        password_hash: hash.update(password, "utf-8").digest("base64")
+      }
+    }
+  })
+
+  server.route({
+    path: "/{a*}", // Bebas penandanya
     method: "*",
     handler: (req, res)=> {
       return {
